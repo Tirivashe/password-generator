22408d7b20899d58d8025d736bf31907210087d1
Tirivashe <shaymusts@gmail.com>
2024-08-07 17:29:20 +0200

    Refactor and added numbers
    Refactored code such that special characters and numbers are dynamically
    added according to the flag values

--- main.go
+++ main.go
@@ -9,22 +9,28 @@ import (
 func main() {
 	var length *int
 	var specialChars *bool
+	var numbers *bool
 
 	length = flag.Int("l", 32, "length of password")
 	specialChars = flag.Bool("s", false, "use special characters")
+	numbers = flag.Bool("n", false, "use numbers")
+
 	flag.Parse()
 
-	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
-	alphabetWithSpecialCharacters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:<>?[]123456789"
+	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	if *specialChars {
+		alphabet += "!@#$%^&*()_+"
+	}
+
+	if *numbers {
+		alphabet += "0123456789"
+	}
 
 	var password string
 
 	for i := 0; i < *length; i++ {
-		if *specialChars {
-			password += string(alphabetWithSpecialCharacters[rand.Intn(len(alphabetWithSpecialCharacters))])
-		} else {
-			password += string(alphabet[rand.Intn(len(alphabet))])
-		}
+		password += string(alphabet[rand.Intn(len(alphabet))])
 	}
 
 	fmt.Printf("Your password is: %s\n", password)
