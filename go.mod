20f46158556f4ad7c06b505ab63ba9377ca42ce9
Tirivashe <shaymusts@gmail.com>
2024-08-07 13:00:28 +0200

    Initial commit
    Complete application in an initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tirivashe/password-generator
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var length *int
+	var specialChars *bool
+
+	length = flag.Int("l", 32, "length of password")
+	specialChars = flag.Bool("s", false, "use special characters")
+	flag.Parse()
+
+	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	alphabetWithSpecialCharacters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:<>?[]"
+
+	var password string
+
+	for i := 0; i < *length; i++ {
+		if *specialChars {
+			password += string(alphabetWithSpecialCharacters[rand.Intn(len(alphabetWithSpecialCharacters))])
+		} else {
+			password += string(alphabet[rand.Intn(len(alphabet))])
+		}
+	}
+
+	fmt.Printf("Your password is: %s\n", password)
+
+}
